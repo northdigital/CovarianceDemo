a21ad42bf59e57e64d01fc24b62f7139ac797daf
northdigital <10321991+northdigital@users.noreply.github.com>
2020-12-21 11:13:00 +0200

    commit

--- Tutor06.cs
+++ Tutor06.cs
@@ -6,7 +6,7 @@ namespace Tutor06
   interface IMyInterface<out R, P> where P : Animal
                                    where R : P
   {
-    R Test(P t); // the error R cannot be used as a parameter is fixed.
+    R Test(P p); // the error R cannot be used as a parameter is fixed.
   }
 
   class MyAnimalClass : IMyInterface<Animal, Animal> { public Animal Test(Animal p) { return p; } }
@@ -21,7 +21,7 @@ namespace Tutor06
       { IMyInterface<Dog, Animal> v = new MyAnimalClass(); } // MyAnimalClass returns Animal not a Dog
       { IMyInterface<Dog, Dog> v = new MyAnimalClass(); } // requires a dog parameter && must return Dog or inherited
 
-      { IMyInterface<Animal, Animal> v = new MyDogClass(); } // MyDog requires a Dog parameter & returns a Dog
+      { IMyInterface<Animal, Animal> v = new MyDogClass(); } // MyDog requires a Dog parameter
       { IMyInterface<Animal, Dog> v = new MyDogClass(); } // <-> Animal cannot be converted to Dog (where)
       { IMyInterface<Dog, Animal> v = new MyDogClass(); } // MyDog requires a Dog parameter and Animal cannot be converted to Dog
       { IMyInterface<Dog, Dog> v = new MyDogClass(); }
